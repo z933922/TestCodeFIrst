310b86f4435ec0e14f38e9416114ed83edb4bb6a
z933922 <xiangyueqixi@foxmail.com>
2017-03-15 17:06:36 +0800

    添加项目文件。

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+        }
+
+         public static (string a,string b,string c) Sb()
+        {
+            return ("a", "b", "c");
+        }
+    }
+}
